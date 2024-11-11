90288ea53e0b26aac977ef3af5ee874a4e45c5b7
Neeraj Kumar K <neerajnithin55@gmail.com>
2024-11-11 15:50:22 +0530

    initial commit

--- program1.py
+++ program1.py
@@ -4,7 +4,26 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        pass
+        # Dictionary to store matching pairs of brackets
+        bracket_map = {')': '(', '}': '{', ']': '['}
+        
+        # Stack to keep track of opening brackets
+        stack = []
+        
+        # Iterate over each character in the string
+        for char in s:
+            if char in bracket_map.values():
+                # If it's an opening bracket, push it to the stack
+                stack.append(char)
+            elif char in bracket_map.keys():
+                # If it's a closing bracket, check for matching opening bracket
+                if not stack or stack[-1] != bracket_map[char]:
+                    return False  # If no match, return False
+                stack.pop()  # Pop the matching opening bracket from the stack
+        
+        # If the stack is empty, all brackets are matched
+        return len(stack) == 0
+
 
 
 
--- program2.py
+++ program2.py
@@ -4,7 +4,26 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        pass
+        # Dictionary to map Roman numerals to integers
+        roman_to_int = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000
+        }
+        
+        total = 0
+        length = len(s)
+        
+        # Traverse each character in the string
+        for i in range(length):
+            # If current character is less than the next character, subtract it
+            if i < length - 1 and roman_to_int[s[i]] < roman_to_int[s[i + 1]]:
+                total -= roman_to_int[s[i]]
+            else:
+                # Otherwise, add it to the total
+                total += roman_to_int[s[i]]
+        
+        return total
+
 
 
 
